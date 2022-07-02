49bc41e931dcb0ad1c3bfedbef5a9b2ee993c319
simpleneeraj <simplneeraj@gmail.com>
2022-07-02 19:30:51 +0530

    feature-alpha

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import "styles/globals.scss";
 import App from "app/index";
 import store from "store";
 import { Provider } from "react-redux";
-import reportWebVitals from "worker/webvitals";
-import register from "worker";
+// import reportWebVitals from "worker/webvitals";
+// import register from "worker";
 
 const RootApp = () => {
   return (
@@ -29,8 +29,8 @@ ReactDOM.render(<RootApp />, root);
 
 // serviceWorkerRegistration.register();
 
-register();
+// register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
